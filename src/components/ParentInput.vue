564aa6666e8a7532fd422701480a965ab56bbeb2
NeAleshka <alexeybudai@gmail.com>
2023-05-30 21:05:17 +0300

    add type for $event in input

--- src/components/ParentInput.vue
+++ src/components/ParentInput.vue
@@ -1,33 +1,47 @@
 <template>
   <div class="px-[16px] py-[8px] border-2 rounded-[5px] mb-[10px]">
-    <div class="text-[13px] leading-[16px] text-light-gray">{{props.label}}</div>
-    <input v-if="props.label==='Имя'" :placeholder="`Введите ${props.label.toLowerCase()} родителя`" class="w-full outline-0"
-           v-model.trim="inputValue"
-           @input="changeValue(props.label,$event.target.value)">
-    <input v-else :placeholder="`Введите ${props.label.toLowerCase()} родителя`" class="w-full outline-0"
-           type="number"
-           v-model.number="inputValue"
-           @input="changeValue(props.label,$event.target.value)">
+    <div class="text-[13px] leading-[16px] text-light-gray">
+      {{ props.label }}
+    </div>
+    <input
+      v-if="props.label === 'Имя'"
+      :placeholder="`Введите ${props.label.toLowerCase()} родителя`"
+      class="w-full outline-0"
+      v-model.trim="inputValue"
+      @input="
+        changeValue(props.label, ($event.target as HTMLInputElement).value)
+      "
+    />
+    <input
+      v-else
+      :placeholder="`Введите ${props.label.toLowerCase()} родителя`"
+      class="w-full outline-0"
+      type="number"
+      v-model.number="inputValue"
+      @input="
+        changeValue(props.label, ($event.target as HTMLInputElement).value)
+      "
+    />
   </div>
 </template>
 
-<script lang=ts setup>
-import {defineProps,ref,defineEmits,defineExpose} from 'vue'
+<script lang="ts" setup>
+import { ref } from "vue";
 //import defineProps for Webstorm,because it not found
-const props=defineProps({
-  label:{
-    type:String,
-    required:true
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
   },
-})
-const inputValue=ref('')
+});
+const inputValue = ref("");
 const emits = defineEmits<{
-  changeInputValue: [label: string,value:string]
-}>()
+  changeInputValue: [label: string, value: string];
+}>();
 
-const changeValue=(label:string,value:string)=>{
-  emits('changeInputValue',label,value)
-}
+const changeValue = (label: string, value: string) => {
+  emits("changeInputValue", label, value);
+};
 
-defineExpose({inputValue})
+defineExpose({ inputValue });
 </script>
--- src/pages/PreviewPage.vue
+++ src/pages/PreviewPage.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="w-full px-[20px] pt-[30px] flex items-center ">
+  <div class="w-full px-[20px] pt-[30px] flex items-center">
     <div class="w-[616px] mx-auto overflow-y-auto">
-      <div class="w-full mb-[33px] flex flex-wrap justify-between ">
-        <preview-item v-for="item in itemsArray" :parentName="item.name" :parentAge="item.age" :children="item.children"/>
+      <div class="w-full mb-[33px] flex flex-wrap justify-between">
+        <preview-item
+          v-for="item in itemsArray"
+          :parentName="item.name"
+          :parentAge="item.age"
+          :children="item.children"
+        />
+      </div>
     </div>
   </div>
-
-  </div>
-
 </template>
 
-<script lang=ts setup>
-import {PARENT_ARRAY_KEY} from '../keys'
-import {IParent} from "../interfaces";
-import PreviewItem from '../components/PreviewItem.vue'
+<script lang="ts" setup>
+import { PARENT_ARRAY_KEY } from "../keys";
+import { IParent } from "../interfaces";
+import PreviewItem from "../components/PreviewItem.vue";
 
-
-const itemsArray: IParent[] = JSON.parse(localStorage.getItem(PARENT_ARRAY_KEY)) || []
-
-
+const itemsArray: IParent[] =
+  JSON.parse(localStorage.getItem(PARENT_ARRAY_KEY) as string) || [];
 </script>
-
